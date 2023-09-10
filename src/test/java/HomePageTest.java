e4a355315cbe398d350b38d48e523614724f0a55
Abdelrahman <abod.deba.000@gmail.com>
2023-09-10 16:36:49 +0300

    test file

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -12,7 +12,7 @@ import static java.lang.Thread.sleep;
 
 public class HomePageTest {
     WebDriver driver;
-    WebElement contactButton;
+
     JavascriptExecutor jse;
     @BeforeClass
     public void setUp(){
@@ -21,48 +21,21 @@ public class HomePageTest {
         options.addArguments("start-maximized");
         driver=  WebDriverManager.chromedriver().capabilities(options).create();
         jse = (JavascriptExecutor) driver;
-        driver.get("https://wewill.tech");
+        driver.get("https://famcare.app/");
 
     }
+
+
     @Test(priority = 1)
-    public void contactButtonIsDisplayed()  {
-        contactButton=driver.findElement(By.xpath("//*[@id=\"ast-desktop-header\"]/div[1]/div/div/div/div[2]/div[2]/div"));
-        Assert.assertTrue(contactButton.isDisplayed());
+    public void bookFirstSessionButtonIsDisplayed(){
+        WebElement bookFirstSession = driver.findElement(By.xpath("//*[@id=\"heroText\"]/div/div[3]/a"));
+            Assert.assertTrue(bookFirstSession.isDisplayed());
     }
-
     @Test(priority = 2)
     public void contactButtonTextIsCorrect(){
-        Assert.assertEquals(contactButton.getText(),"Contact");
-    }
+        WebElement bookFirstSession = driver.findElement(By.xpath("//*[@id=\"heroText\"]/div/div[3]/a"));
 
-    @Test(priority = 4)
-    public void scrollTopIsDisplayed() throws InterruptedException {
-        jse.executeScript("window.scrollBy(0,500)");
-        sleep(3000);
-        WebElement scrollTop= driver.findElement(By.cssSelector("svg.ast-arrow-svg"));
-        Assert.assertTrue(scrollTop.isDisplayed());
-
-    }
-    @Test(priority = 5)
-    public void sliderButtonNextIsDisplayed()  {
-        WebElement sliderNextButton = driver.findElement(By.className("swiper-button-next"));
-        Assert.assertTrue(sliderNextButton.isDisplayed());
-    }
-    @Test(priority = 6)
-    public void sliderButtonPreviousIsDisplayed()  {
-        WebElement sliderPreviousButton = driver.findElement(By.className("swiper-button-prev"));
-        Assert.assertTrue(sliderPreviousButton.isDisplayed());
-    }
-    WebElement productsTitle;
-    @Test(priority = 7)
-    public void productsTitleFooterIsDisplayed()  {
-         productsTitle=driver.findElement(By.xpath("//*[@id=\"nav_menu-3\"]/h2"));
-        Assert.assertTrue(productsTitle.isDisplayed());
-    }
-
-    @Test(priority = 8)
-    public void productsTitleFooterTextIsCorrect(){
-        Assert.assertEquals(productsTitle.getText(),"Product");
+        Assert.assertEquals(bookFirstSession.getText(),"احجز جلستك الأولى" );
     }
 
 }
