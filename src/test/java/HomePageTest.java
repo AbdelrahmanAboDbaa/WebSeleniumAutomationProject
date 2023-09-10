4ba4c2a3b9b1b0ecc1fe30b4c1302a433065863e
Abdelrahman <abod.deba.000@gmail.com>
2023-09-10 15:24:09 +0300

    hello

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,2 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
+
 public class HomePageTest {
+    WebDriver driver;
+    WebElement contactButton;
+    JavascriptExecutor jse;
+    @BeforeClass
+    public void setUp(){
+        //Initialize driver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver=  WebDriverManager.chromedriver().capabilities(options).create();
+        jse = (JavascriptExecutor) driver;
+        driver.get("https://wewill.tech");
+
+    }
 }
