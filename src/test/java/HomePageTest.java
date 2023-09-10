1d1f1602e48906ecb4aadfc5007c0af199a07033
Abdelrahman <abod.deba.000@gmail.com>
2023-09-10 15:31:12 +0300

    hello

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,9 +1,14 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static java.lang.Thread.sleep;
 
 public class HomePageTest {
     WebDriver driver;
@@ -19,4 +24,34 @@ public class HomePageTest {
         driver.get("https://wewill.tech");
 
     }
+    @Test(priority = 1)
+    public void contactButtonIsDisplayed()  {
+        contactButton=driver.findElement(By.xpath("//*[@id=\"ast-desktop-header\"]/div[1]/div/div/div/div[2]/div[2]/div"));
+        Assert.assertTrue(contactButton.isDisplayed());
+    }
+
+    @Test(priority = 2)
+    public void contactButtonTextIsCorrect(){
+        Assert.assertEquals(contactButton.getText(),"Contact");
+    }
+
+    @Test(priority = 4)
+    public void scrollTopIsDisplayed() throws InterruptedException {
+        jse.executeScript("window.scrollBy(0,500)");
+        sleep(3000);
+        WebElement scrollTop= driver.findElement(By.cssSelector("svg.ast-arrow-svg"));
+        Assert.assertTrue(scrollTop.isDisplayed());
+
+    }
+    @Test(priority = 5)
+    public void sliderButtonNextIsDisplayed()  {
+        WebElement sliderNextButton = driver.findElement(By.className("swiper-button-next"));
+        Assert.assertTrue(sliderNextButton.isDisplayed());
+    }
+    @Test(priority = 6)
+    public void sliderButtonPreviousIsDisplayed()  {
+        WebElement sliderPreviousButton = driver.findElement(By.className("swiper-button-prev"));
+        Assert.assertTrue(sliderPreviousButton.isDisplayed());
+    }
+
 }
